f4bee676e0b2cb94a119f36ede9d80a79629cbfd
kareem2241 <153555176+kareem2241@users.noreply.github.com>
2023-12-12 02:19:23 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+201016865749']
+global.premium = ['+201016865749']
+global.ownernomer = '+201016865749'
+global.ownername = '🐦Draken'
+global.botname = 'Kareem'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
